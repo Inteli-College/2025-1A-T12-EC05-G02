3c833b5778219997ac99d6f76cb8bb63e196d432
HeitorCand <heitor.candido@sou.inteli.edu.br>
2025-03-10 13:14:38 -0300

    feat: .env

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 from user.user import usersFlask
 from robot.robot import robotFlask
+from dotenv import load_dotenv
 
 # python3 -m flask --app main run
 
+load_dotenv()
 app = Flask(__name__)
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres.mrlralmljljhiynqbshr:w0WwQGgXW99zaGxh@aws-0-us-west-1.pooler.supabase.com:5432/postgres"
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 
 db = SQLAlchemy()
 db.init_app(app)
