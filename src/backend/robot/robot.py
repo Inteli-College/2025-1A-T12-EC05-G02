2b9d5ad9892ea727788ab21ac7568e5cc10b5f43
HeitorCand <heitor.candido@sou.inteli.edu.br>
2025-03-11 17:05:23 -0300

    feat: log back

--- src/backend/robot/robot.py
+++ src/backend/robot/robot.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, current_app
 from flask_socketio import emit, send
-from extensions import socketio
+from extensions import socketio, db
+from models.log_sistema import LogSistema
 # from main import socketio
 
 robotFlask = Blueprint('robot', __name__, url_prefix='/robot')
@@ -17,8 +18,19 @@ def handle_disconnect():
     print("client has disconnected")
     emit("disconnectResponse", {"data": f"id: {request.sid} is disconnected"})
 
-@socketio.on('data')
+@socketio.on('log')
 def handle_message(data):
-    print("data from the front end: ", str(data))
-    emit("data", {'data': data, 'id': request.sid}, broadcast=True)
+    print("log: ", str(data))
+    try:
+        ultimo_log = db.session.query(LogSistema).order_by(LogSistema.id.desc()).first()
+        proximo_id = (ultimo_log.id + 1) if ultimo_log else 1
+        log = LogSistema(id=proximo_id, acao=data['acao'], detalhes=data['detalhes'], usuario_id=int(data['usuario_id']))
+        db.session.add(log)
+        db.session.commit()
+        print("log added to database")
+    except Exception as e:
+        db.session.rollback()
+        print(f"Error adding log to database: {e}")
+    print("log added to database")
+    
     
