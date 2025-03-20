eedff419075687509abc58edd239c558b7916ea3
matheusjorgerosa <matheus.rosa@sou.inteli.edu.br>
2025-03-20 12:47:31 -0300

    feat: adiciona redirecionamento aos botoes

--- src/frontend/pharmabot/src/app/home/page.tsx
+++ src/frontend/pharmabot/src/app/home/page.tsx
@@ -1,27 +1,50 @@
-"use client"
+"use client";
 
-import SidebarActions from '../components/home/sidebar-actions';
-import StatusCard from '../components/home/subcomponents/status-card';
-
-const handleClick = () => {
-    console.log("click");
-};
+import SidebarActions from "../components/home/sidebar-actions";
+import StatusCard from "../components/home/subcomponents/status-card";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const navigate = (path: string) => {
+    redirect(path);
+  };
+
   return (
-    <div className='bg-[#FBFBFB] w-full h-full pl-8 pr-8'>
-      <div className='flex flex-col w-full h-[275px] items-center justify-center'>
-        <img src="./pharmatech-logo-lg.svg" alt="Logo PharmaTech" className='w-[668px] h-[183px] object-cover'/>
+    <div className="bg-[#FBFBFB] w-full h-full pl-8 pr-8">
+      <div className="flex flex-col w-full h-[275px] items-center justify-center">
+        <img
+          src="./pharmatech-logo-lg.svg"
+          alt="Logo PharmaTech"
+          className="w-[668px] h-[183px] object-cover"
+        />
       </div>
-      <div className='flex flex-row gap-x-4'>
-        <div className='flex flex-col w-[33%] justify-center items-center'>
+      <div className="flex flex-row gap-x-4">
+        <div className="flex flex-col w-[33%] justify-center items-center">
           <SidebarActions />
         </div>
-        <div className='flex flex-col w-[100%]items-center bg-white rounded-md shadow-md w-[100%] h-[100%] pl-16 pr-16 pt-4 pb-4'>
-          <div className='flex flex-row w-[100%] justify-between'>
-              <StatusCard urgency={0} quantity={21} onClick={handleClick}/>
-              <StatusCard urgency={1} quantity={39} onClick={handleClick}/>
-              <StatusCard urgency={2} quantity={10} onClick={handleClick}/>
+        <div className="flex flex-col w-[100%]items-center bg-white rounded-md shadow-md w-[100%] h-[100%] pl-16 pr-16 pt-4 pb-4">
+          <div className="flex flex-row w-[100%] justify-between">
+            <StatusCard
+              urgency={0}
+              quantity={21}
+              onClick={() => {
+                navigate("/login");
+              }}
+            />
+            <StatusCard
+              urgency={1}
+              quantity={39}
+              onClick={() => {
+                navigate("/login");
+              }}
+            />
+            <StatusCard
+              urgency={2}
+              quantity={10}
+              onClick={() => {
+                navigate("/login");
+              }}
+            />
           </div>
         </div>
       </div>
