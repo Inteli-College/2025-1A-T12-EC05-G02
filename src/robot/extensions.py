3ccf2eefa95ab21dfe2517d474d24ba8a1196dd7
Heitor Candido <heitor.candido@sou.inteli.edu.br>
2025-03-21 14:55:34 -0300

    hotfix: url websocket

--- src/robot/extensions.py
+++ src/robot/extensions.py
@@ -2,4 +2,4 @@ import socketio
 
 # Cria uma instância do cliente Socket.IO
 sio = socketio.Client()
-sio.connect('http://localhost:5555') # Conecta ao servidor Socket.IO
+sio.connect('http://10.32.0.8:6001') # Conecta ao servidor Socket.IO
