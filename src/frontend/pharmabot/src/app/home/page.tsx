a2e6313f7c4d798b1292fb74d07272b662c7baa5
matheusjorgerosa <matheus.rosa@sou.inteli.edu.br>
2025-03-21 13:05:19 -0300

    feat: corrige inclusao de tabela dinamica

--- src/frontend/pharmabot/src/app/home/page.tsx
+++ src/frontend/pharmabot/src/app/home/page.tsx
@@ -3,15 +3,15 @@
 import SidebarActions from "../components/home/sidebar-actions";
 import StatusCard from "../components/home/status-card";
 import TabelaPharma from "../components/TabelaPharma";
-import SelectButton from '../components/SelectButton';
-import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
-import Stack from '@mui/material/Stack';
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import { exportToCSV } from '../(util)/exportToCSV';
+import SelectButton from "../components/SelectButton";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import { exportToCSV } from "../(util)/exportToCSV";
 import { redirect } from "next/navigation";
-import { useState, useEffect } from 'react';
-import { Column } from '../components/table';
+import { useState, useEffect } from "react";
+import { Column } from "../components/table";
 
 interface Data {
   id: string;
@@ -22,12 +22,17 @@ interface Data {
 }
 
 const colunas: Column[] = [
-    { id: 'id', label: 'ID', minWidth: 100 },
-    { id: 'horaPrescricao', label: 'Horário de Separação', minWidth: 150, format: (value: Date) => value.toLocaleString('pt-BR') },
-    { id: 'prescricao', label: 'Prescrição', minWidth: 170 },
-    { id: 'paciente', label: 'Paciente', minWidth: 200 },
-    { id: 'farmaceutico', label: 'Farmacêutico', minWidth: 170},
-  ];
+  { id: "id", label: "ID", minWidth: 100 },
+  {
+    id: "horaPrescricao",
+    label: "Horário de Separação",
+    minWidth: 150,
+    format: (value: Date) => value.toLocaleString("pt-BR"),
+  },
+  { id: "prescricao", label: "Prescrição", minWidth: 170 },
+  { id: "paciente", label: "Paciente", minWidth: 200 },
+  { id: "farmaceutico", label: "Farmacêutico", minWidth: 170 },
+];
 
 export default function Home() {
   const navigate = (path: string) => {
@@ -57,8 +62,10 @@ export default function Home() {
       ? `http://127.0.0.1:5555/api/user/prescriptions?acao=${selectedAcao}`
       : rota;
 
+    console.log(url);
+
     fetch(url)
-      .then((response) => response.json()
+      .then((response) => response.json())
       .then((data) => {
         // Transformar os dados no formato esperado
         const formattedData: Data[] = data.Logs.map((item: any) => ({
@@ -72,7 +79,7 @@ export default function Home() {
         setRows(formattedData);
       })
       .catch((error) => console.error("Erro ao buscar logs:", error))
-      .finally(() => setLoading(false))); // Finaliza o carregamento
+      .finally(() => setLoading(false)); // Finaliza o carregamento
   }, [selectedAcao, key]); // O efeito será executado sempre que `selectedAcao` mudar
 
   // Filtra os dados com base no texto de pesquisa
@@ -143,33 +150,64 @@ export default function Home() {
             </div>
           </div>
           <div className="flex flex-col justify-center items-center w-full h-full">
-          <TabelaPharma titulo="Histórico de Ações do Sistema"
-            subtitulo="Aqui você encontra o histórico de ações do sistema, como início de separações e recebimentos de pedidos" rows={filteredRows} render={key} loading={loading} colunas={colunas}>
-            <div className='flex justify-between items-center'>
-                <Stack id="pesquisar" spacing={1} direction="row" className='items-center'>
-                    <TextField
-                        label="Pesquisar"
-                        size='small'
-                        type="search"
-                        value={searchText}
-                        onChange={handleSearchChange}
-                    />
-                    <span></span>
-                    <SelectButton
-                        atributo='acao'
-                        label='Ação'
-                        onSelect={handleAcaoChange}
-                        render={key}
-                        rota={rota}
-                    />
-                    <FilterAltIcon className='opacity-70' />
+            <TabelaPharma
+              titulo="Histórico de Ações do Sistema"
+              subtitulo="Aqui você encontra o histórico de ações do sistema, como início de separações e recebimentos de pedidos"
+              rows={filteredRows}
+              render={key}
+              loading={loading}
+              colunas={colunas}
+            >
+              <div className="flex justify-between items-center">
+                <Stack
+                  id="pesquisar"
+                  spacing={1}
+                  direction="row"
+                  className="items-center"
+                >
+                  <TextField
+                    label="Pesquisar"
+                    size="small"
+                    type="search"
+                    value={searchText}
+                    onChange={handleSearchChange}
+                  />
+                  <span></span>
+                  <SelectButton
+                    atributo="acao"
+                    label="Ação"
+                    onSelect={handleAcaoChange}
+                    render={key}
+                    rota={rota}
+                  />
+                  <FilterAltIcon className="opacity-70" />
                 </Stack>
                 <Stack id="botoes" spacing={1} direction="row">
-                    <Button variant="outlined" color="black" onClick={reRender}>Atualizar</Button>
-                    <Button variant="contained" onClick={() => exportToCSV(filteredRows, ["ID", "Data e Hora", "Ação", "Detalhes", "Responsável"], ['id', 'dataHora', 'acao', 'detalhes', 'responsavel'], "historico-acoes")}>Exportar CSV</Button>
+                  <Button variant="outlined" color="black" onClick={reRender}>
+                    Atualizar
+                  </Button>
+                  <Button
+                    variant="contained"
+                    onClick={() =>
+                      exportToCSV(
+                        filteredRows,
+                        [
+                          "ID",
+                          "Data e Hora",
+                          "Ação",
+                          "Detalhes",
+                          "Responsável",
+                        ],
+                        ["id", "horaPrescricao", "prescricao", "paciente", "farmaceutico"],
+                        "home"
+                      )
+                    }
+                  >
+                    Exportar CSV
+                  </Button>
                 </Stack>
-            </div>
-        </TabelaPharma>
+              </div>
+            </TabelaPharma>
           </div>
         </div>
       </div>
