f4ecd1ac688a2e929206604952752b311d2540a7
Felipe <felipe.simao@sou.inteli.edu.br>
2025-03-25 17:53:47 -0300

    feat: criacao da rota

--- src/backend/logs/logs.py
+++ src/backend/logs/logs.py
@@ -1,15 +1,41 @@
 # Importando as bibliotecas necessárias
-import bcrypt
-from sqlalchemy.exc import IntegrityError
-from models.usuario import Usuario
-from models.log_sistema import LogSistema
-from datetime import datetime
+from flask import Blueprint, jsonify
 from extensions import db
-from flask import Blueprint, request, jsonify
-from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, unset_jwt_cookies
-from decorators.route_auth import role_required
-from sqlalchemy import desc
+from models.estoque import Estoque
+from models.medicamento import Medicamento
 
 
 # Definindo o Blueprint
 logsFlask = Blueprint('logs', __name__, url_prefix='/logs')
+
+@logsFlask.route('/estoque', methods=["GET"])
+def listar_estoque():
+    session = db.session
+    try:
+        # Consulta unindo as tabelas Estoque e Medicamento
+        query = session.query(
+            Estoque.id,
+            Estoque.medicamento_id,
+            Estoque.quantidade,
+            Estoque.bin_localizacao,
+            Estoque.ultima_atualizacao,
+            Medicamento.nome  # Pegando o nome do medicamento correspondente
+        ).join(Medicamento, Estoque.medicamento_id == Medicamento.id)
+
+        # Converte os resultados para uma lista de dicionários
+        estoque_list = [
+            {
+                "id": item.id,
+                "medicamento_id": item.medicamento_id,
+                "nome_medicamento": item.nome,
+                "quantidade": item.quantidade,
+                "bin_localizacao": item.bin_localizacao,
+                "ultima_atualizacao": item.ultima_atualizacao
+            }
+            for item in query.all()
+        ]
+        
+    finally:
+        session.close()
+
+    return {"estoque": estoque_list}, 200
