e19c83e9c7d7a0cb7dff7fb3351b45a36ee5584b
julia-lika <julia.ishikawa@sou.inteli.edu.br>
2025-03-26 19:27:50 -0300

    fix: realiza correcoes na integraçao com back

--- src/frontend/pharmabot/src/app/usuario/page.tsx
+++ src/frontend/pharmabot/src/app/usuario/page.tsx
@@ -45,10 +45,10 @@ export default function UsuariosCadastrados() {
 useEffect(() => {
     setLoading(true);
   
-    fetch("http://127.0.0.1:5555/api/user/list")
+    fetch("http://127.0.0.1:5555/user/list")
       .then(res => res.json())
       .then(data => {
-        const formatted: Data[] = data.usuarios.map((user: any) => ({
+        const formatted: Data[] = data.Usuários.map((user: any) => ({
           id: user.id,
           usuario: user.nome,
           email: user.email,
