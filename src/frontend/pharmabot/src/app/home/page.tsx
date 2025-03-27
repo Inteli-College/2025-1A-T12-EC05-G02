1bf36eb6029705ef5e433a04db1c9ecc8adb44c4
Lucas-nepomuceno <lucas.santos@sou.inteli.edu.br>
2025-03-27 16:56:42 -0300

    feat: pesquisa por data na home

--- src/frontend/pharmabot/src/app/home/page.tsx
+++ src/frontend/pharmabot/src/app/home/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs, { Dayjs } from "dayjs";
 import SidebarActions from "../components/home/sidebar-actions";
 import StatusCard from "../components/home/status-card";
 import TabelaPharma from "../components/TabelaPharma";
-import SelectButton from "../components/SelectButton";
 import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
 import Stack from "@mui/material/Stack";
-import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import { exportToCSV } from "../(util)/exportToCSV";
 import { redirect } from "next/navigation";
 import { useState, useEffect } from "react";
 import { Column } from "../components/table";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 interface Data {
   id: string;
@@ -51,6 +53,22 @@ export default function Home() {
     completed: 0,
     in_queue: 0,
   });
+  const [searchDate, setSearchDate] = useState<Dayjs | null>(null);
+
+  const handleDateChange = (newDate: Dayjs | null) => {
+    setSearchDate(newDate);
+  };
+
+  useEffect(() => {
+    if (!searchDate) {
+      setFilteredRows(rows); // Se não houver data, mostra tudo
+    } else {
+      const filtered = rows.filter((row) =>
+        dayjs(row.horaPrescricao).isSame(searchDate, "day")
+      );
+      setFilteredRows(filtered);
+    }
+  }, [searchDate, rows]);
 
   //função para atualizar a página com base no botão atualizar
   const reRender = () => {
@@ -106,11 +124,6 @@ export default function Home() {
             row.farmaceutico
               .toString()
               .toLowerCase()
-              .includes(searchText.toLowerCase())) ||
-          (String(row.horaPrescricao) &&
-            String(row.farmaceutico)
-              .toString()
-              .toLowerCase()
               .includes(searchText.toLowerCase()))
         );
       });
@@ -134,97 +147,107 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="bg-[#FBFBFB] w-full h-[100vh] pl-8 pr-8">
-      <div className="flex flex-col w-full h-[15%] items-center justify-center mb-4 mt-4">
-        <img
-          className="w-[100%] h-[100%] object-contain"
-          src="./pharmatech-logo-lg.svg"
-          alt="Logo PharmaTech"
-        />
-      </div>
-      <div className="flex flex-row gap-x-10">
-        <div className="flex flex-col w-[25%] justify-center items-center">
-          <SidebarActions />
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+
+      <div className="bg-[#FBFBFB] w-full h-[100vh] pl-8 pr-8">
+        <div className="flex flex-col w-full h-[15%] items-center justify-center mb-4 mt-4">
+          <img
+            className="w-[100%] h-[100%] object-contain"
+            src="./pharmatech-logo-lg.svg"
+            alt="Logo PharmaTech"
+          />
         </div>
-        <div className="flex flex-col w-[75%] justify-center items-center ">
-          <div className="flex flex-col w-[100%]items-center bg-white rounded-md shadow-md w-[100%] pl-16 pr-16 pt-4 pb-4">
-            <div className="flex flex-row w-[100%] justify-between gap-x-8">
-              <StatusCard
-                urgency={0}
-                quantity={statuses.in_queue}
-                onClick={() => {
-                  navigate("/kanban");
-                }}
-              />
-              <StatusCard
-                urgency={1}
-                quantity={statuses.completed}
-                onClick={() => {
-                  navigate("/kanban");
-                }}
-              />
+        <div className="flex flex-row gap-x-10">
+          <div className="flex flex-col w-[25%] justify-center items-center">
+            <SidebarActions />
+          </div>
+          <div className="flex flex-col w-[75%] justify-center items-center ">
+            <div className="flex flex-col w-[100%]items-center bg-white rounded-md shadow-md w-[100%] pl-16 pr-16 pt-4 pb-4">
+              <div className="flex flex-row w-[100%] justify-between gap-x-8">
+                <StatusCard
+                  urgency={0}
+                  quantity={statuses.in_queue}
+                  onClick={() => {
+                    navigate("/kanban");
+                  }}
+                />
+                <StatusCard
+                  urgency={1}
+                  quantity={statuses.completed}
+                  onClick={() => {
+                    navigate("/kanban");
+                  }}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col justify-center items-center w-full h-full">
+              <TabelaPharma
+                titulo="Histórico de Prescrições do Sistema"
+                subtitulo="Histórico de prescrições do sistema, como hora de separações, seus respectivos pacientes e farmacêuticos."
+                rows={filteredRows}
+                render={key}
+                itemsPerPage={[5]}
+                initialNumItems={5}
+                loading={loading}
+                colunas={colunas}
+              >
+                <div className="flex justify-between items-center">
+                  <Stack
+                    id="pesquisar"
+                    spacing={1}
+                    direction="row"
+                    className="items-center"
+                  >
+                    <TextField
+                      label="Pesquisar"
+                      size="small"
+                      type="search"
+                      value={searchText}
+                      onChange={handleSearchChange}
+                    />
+                    <span></span>
+                    <DatePicker
+                      label="Filtrar por Data"
+                      value={searchDate || null}  /* Evita data de hoje quando null */
+                      onChange={handleDateChange}
+                      format="DD/MM/YYYY"
+                      slotProps={{ textField: { size: "small" } }}
+                    />
+                  </Stack>
+                  <Stack id="botoes" spacing={1} direction="row">
+                    <Button variant="outlined" color="black" onClick={reRender}>
+                      Atualizar
+                    </Button>
+                    <Button
+                      variant="contained"
+                      onClick={() =>
+                        exportToCSV(
+                          filteredRows,
+                          [
+                            "Prescrição",
+                            "Horário de Separação",
+                            "Paciente",
+                            "Farmacêutico",
+                          ],
+                          [
+                            "prescricao",
+                            "horaPrescricao",
+                            "paciente",
+                            "farmaceutico",
+                          ],
+                          "historico_prescricoes"
+                        )
+                      }
+                    >
+                      Exportar CSV
+                    </Button>
+                  </Stack>
+                </div>
+              </TabelaPharma>
             </div>
           </div>
-          <div className="flex flex-col justify-center items-center w-full h-full">
-            <TabelaPharma
-              titulo="Histórico de Prescrições do Sistema"
-              subtitulo="Histórico de prescrições do sistema, como hora de separações, seus respectivos pacientes e farmacêuticos."
-              rows={filteredRows}
-              render={key}
-              itemsPerPage={[5]}
-              initialNumItems={5}
-              loading={loading}
-              colunas={colunas}
-            >
-              <div className="flex justify-between items-center">
-                <Stack
-                  id="pesquisar"
-                  spacing={1}
-                  direction="row"
-                  className="items-center"
-                >
-                  <TextField
-                    label="Pesquisar"
-                    size="small"
-                    type="search"
-                    value={searchText}
-                    onChange={handleSearchChange}
-                  />
-                  <span></span>
-                </Stack>
-                <Stack id="botoes" spacing={1} direction="row">
-                  <Button variant="outlined" color="black" onClick={reRender}>
-                    Atualizar
-                  </Button>
-                  <Button
-                    variant="contained"
-                    onClick={() =>
-                      exportToCSV(
-                        filteredRows,
-                        [
-                          "Prescrição",
-                          "Horário de Separação",
-                          "Paciente",
-                          "Farmacêutico",
-                        ],
-                        [
-                          "prescricao",
-                          "horaPrescricao",
-                          "paciente",
-                          "farmaceutico",
-                        ],
-                        "historico_prescricoes"
-                      )
-                    }
-                  >
-                    Exportar CSV
-                  </Button>
-                </Stack>
-              </div>
-            </TabelaPharma>
-          </div>
         </div>
       </div>
-    </div>
+    </LocalizationProvider>
   );
 }
