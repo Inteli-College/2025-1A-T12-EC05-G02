17ad26a4df37fb6cb14b48f3f703dffb3a06373c
HeitorCand <heitor.candido@sou.inteli.edu.br>
2025-03-31 17:02:35 -0300

    feat: env robot

--- src/ngrok_start.sh
+++ src/ngrok_start.sh
@@ -28,6 +28,7 @@ echo "Seu domínio do Ngrok é: $NGROK_URL"
 
 # Cria um .env com a URL do Ngrok
 echo "API_URL='$NGROK_URL'" > /home/ec05-g2/2025-1A-T12-EC05-G02/src/frontend/pharmabot/.env
+echo "API_URL='$NGROK_URL'" > /home/ec05-g2/2025-1A-T12-EC05-G02/src/robot/.env
 
 # Builda o frontend
 cd /home/ec05-g2/2025-1A-T12-EC05-G02/src/frontend/pharmabot
@@ -47,6 +48,7 @@ ngrok http 80 --config ~/.ngrok2/ngrok-user2.yml > /dev/null &
 sleep 2
 
 # Captura a URL pública do Ngrok do frontend
-NGROK_URL_FRONT=$(curl -s http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+NGROK_URL_FRONT=$(curl -s http://127.0.0.1:4041/api/tunnels | jq -r '.tunnels[0].public_url')
+
 
 echo "Seu domínio do Ngrok do frontend é: $NGROK_URL_FRONT"
