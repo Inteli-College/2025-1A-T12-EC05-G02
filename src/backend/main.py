c0ac59146ae393a4e6855516c9b1b18fdebdab9e
Gustavo-daCosta <gustavo.costa@sou.inteli.edu.br>
2025-04-01 15:57:18 -0300

    feat: autenticação em todas as rotas

--- src/backend/main.py
+++ src/backend/main.py
@@ -3,9 +3,7 @@ import logging
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from flask_socketio import SocketIO
 from dotenv import load_dotenv
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from user.user import usersFlask
 from medicine.medicine import medicineFlask
 from robot.robot import robotFlask
